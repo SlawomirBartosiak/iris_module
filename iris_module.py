c736f0fbd979d579330904c02c71f64afa1ee1f3
SlawomirBartosiak <slawomir.bartosiak1989@gmail.com>
2023-12-29 15:08:34 +0100

    Dodanie docstringów do funkcji w setoda_module.py

--- iris_module.py
+++ iris_module.py
@@ -11,7 +11,7 @@ EXTRACT_PATH = os.path.join(ROOT_DIR, 'data')
 OUTPUT_PATH = os.path.join(ROOT_DIR, 'output_data')
 URL_PATH = "https://archive.ics.uci.edu/static/public/53/iris.zip"
 
-# check if directories exist if not create
+# check if directories exist, if not create
 os.makedirs(EXTRACT_PATH, exist_ok=True)
 os.makedirs(OUTPUT_PATH, exist_ok=True)
 
@@ -71,17 +71,27 @@ class DataProcessor:
         with open(path, newline='', encoding='utf-8') as csvfile:
             reader = csv.reader(csvfile, delimiter=',')
             if header:
-                col_names = next(reader)
+                col_names = next(reader)  # Read column names if header is True
 
             for row in reader:
-                if len(row) != 0:
-                    X.append(row[:4])
-                    y.extend(row[4:])
+                if len(row) != 0:  # check if row contains the data
+                    X.append(row[:4])  # Nested lists for X data
+                    y.extend(row[4:])  # y as one-dimensional list
 
         return X, y, col_names
 
     @staticmethod
     def show_labels(labels=None):
+        """
+        Displays data labels.
+
+        Parameters:
+        labels (list): List of labels.
+
+        Returns:
+        None
+        """
+
         if not labels:
             print("Brak etykiet w pliku: \n", labels)
         else:
@@ -89,6 +99,19 @@ class DataProcessor:
 
     @staticmethod
     def disp_data_slice(X, y, start_idx=None, stop_idx=None):
+        """
+        Displays dataset labels and data within the specified range of indices.
+
+        Parameters:
+        X (list): Data.
+        y (list): Labels.
+        start_idx (int, optional): Starting index of the range to display. Default is None.
+        stop_idx (int, optional): Ending index (exclusive) of the range to display. Default is None.
+
+        Returns:
+        None
+        """
+
         if start_idx is None and stop_idx is None:
             print("\nEtykiety i dane:")
             for i in range(len(y)):
@@ -100,17 +123,35 @@ class DataProcessor:
 
     @staticmethod
     def train_test_split(X, y, train_size=0.7, val_size=0.1, test_size=0.2):
+        """
+        Splits the dataset into training, validation, and test sets based on provided proportions.
+
+        Parameters:
+        X (list): Data.
+        y (list): Labels.
+        train_size (float, optional): The proportion of the dataset to include in the training set. Default is 0.7.
+        val_size (float, optional): The proportion of the dataset to include in the validation set. Default is 0.1.
+        test_size (float, optional): The proportion of the dataset to include in the test set. Default is 0.2.
+
+        Returns:
+        tuple: A tuple containing the split datasets - X_train, X_val, X_test, y_train, y_val, y_test.
+        """
+
+        # Check if provided proportions sum up to 1
         if (train_size + val_size + test_size) != 1:
             raise ValueError("Wartości train_size, val_size i test_size muszą się sumować do 1!")
 
-        random.seed(42)
-        combined_data = list(zip(X, y))
+        # shuffle the data
+        random.seed(42)  # set seed for reproducibility
+        combined_data = list(zip(X, y))  # merge labels and data for shuffle
         random.shuffle(combined_data)
-        X, y = zip(*combined_data)
+        X, y = zip(*combined_data)  # unpack the data
 
+        # calculate split indices
         train_idx = round(train_size * len(y))
         val_idx = round((train_size + val_size) * len(y))
 
+        # split the data
         X_train, X_val, X_test = X[:train_idx], X[train_idx:val_idx], X[val_idx:]
         y_train, y_val, y_test = y[:train_idx], y[train_idx:val_idx], y[val_idx:]
 
@@ -118,12 +159,36 @@ class DataProcessor:
 
     @staticmethod
     def disp_label_counts(y):
+        """
+        Display count of unique classes.
+
+        Parameters:
+        y (list): A list containing class labels.
+
+        Returns:
+        list: A list of tuples containing unique class labels and their respective frequencies.
+        """
+
         unique_labels = list(set(y))
         print("\nKlasy i ich liczba w zbiorze danych: ")
         [print((label, y.count(label))) for label in unique_labels]
 
     @staticmethod
     def disp_selected_label_data(X, y, selected_label="Iris-setosa"):
+        """
+        Display the data of selected label.
+
+        Args:
+        - y (list): List of labels.
+        - selected_label (str, optional): The label for which associated data will be displayed.
+                                     Defaults to "Iris-setosa".
+
+        Example:
+        To display data associated with the label "Iris-versicolor":
+        ```
+        disp_selected_label_data(y=y, selected_label="Iris-versicolor")
+        ```
+        """
         combined_data = list(zip(y, X))
         print("\nDane dla wybranej klasy: ")
         [print(i) for i in combined_data if i[0] == selected_label]
@@ -132,6 +197,16 @@ class DataProcessor:
 class DataSaver:
     @staticmethod
     def save_as_csv(data, file_name):
+        """
+        Save a prepared dataset as a CSV file.
+
+        Parameters:
+        data (list): The list to be saved as a CSV file.
+        file_name (str): The name of the CSV file.
+
+        Returns:
+        None
+        """
         path = os.path.join(OUTPUT_PATH, file_name)
         with open(path, mode='w', newline='') as file:
             writer = csv.writer(file)
@@ -146,8 +221,11 @@ def main():
     DataProcessor.disp_data_slice(X, y, 0, 4)  # example of displaying sliced dataset
 
     # train, test, val split
-    X_train, X_val, X_test, y_train, y_val, y_test = DataProcessor.train_test_split(X=X, y=y, train_size=0.6,
-                                                                                   val_size=0.15, test_size=0.25)
+    X_train, X_val, X_test, y_train, y_val, y_test = DataProcessor.train_test_split(X=X,
+                                                                                    y=y,
+                                                                                    train_size=0.7,
+                                                                                    val_size=0.1,
+                                                                                    test_size=0.2)
     DataProcessor.disp_label_counts(y)  # display label counts
 
     # Display the selected label from the chosen dataset (this one is on test dataset)
@@ -171,6 +249,7 @@ def main():
             DataSaver.save_as_csv(data, file_name)
         except Exception as e:
             print(f"Wystąpił problem podczas zapisu danych do pliku: {e}")
+
 
 if __name__ == "__main__":
     main()
